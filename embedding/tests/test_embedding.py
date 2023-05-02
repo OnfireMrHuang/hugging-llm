6c8f488189e149a370b8e7f6951227367aef734e
huangww01 <huangww01@mingyuanyun.com>
2023-05-02 16:43:12 +0800

    添加模拟测试生成256个维度的embedding

--- embedding/tests/test_embedding.py
+++ embedding/tests/test_embedding.py
@@ -1,12 +1,15 @@
-class TestEmbedding(unittest.TestCase):
-    def __init__(self):
-        pass
+import unittest
+import numpy as np
 
-    def test_embedding(self):
+
+class TestEmbedding(unittest.TestCase):
+    def test_gen_256_dim_embedding(self):
         rng = np.random.default_rng(42)
-        # 词表大小设置N=16,维度D=256
+        # 词表大小N=16,维度D=256
         table = rng.uniform(size=(16, 256))
-        table.shape
-        self.assertEqual(table.shape, (16, 256))
+        print("shape: ")
+        print(table.shape)
+        print("table: ")
+        print(table)
 
 
